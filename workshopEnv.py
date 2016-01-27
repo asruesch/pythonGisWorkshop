096d47b96151223a5931050cf7b066d40a9dc8fd
Aaron Ruesch <asruesch@gmail.com>
2016-01-27 16:34:26 -0600

    updated year in stuff. Added some more informative print statements

--- workshopEnv.py
+++ workshopEnv.py
@@ -4,13 +4,14 @@ import urllib
 import zipfile
 from arcpy import env
 
-url = 'https://dl.dropboxusercontent.com/u/17521862/WLIA_2015_classdata.zip'
+print 'Downloading class data into home directory'
+url = 'https://dl.dropboxusercontent.com/u/17521862/WLIA_2016_classdata.zip'
 home = os.path.expanduser("~")
-print home
-wd = home + '\\pythonWorkshopWLIA2014'
-print wd
+print 'home directory: ' + home
+wd = home + '\\pythonWorkshopWLIA2016'
+print 'working directory: ' + wd
 fgdb = wd + '\\WLIA_pythonclass_data.gdb'
-print fgdb
+print 'class data: ' + fgdb
 
 
 def refresh_data():
